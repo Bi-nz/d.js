b60af16b67e05072f50d976a991a624ff9d93615
Nuno135 <nuriel.taha@outlook.com>
2019-03-14 00:20:15 +0100

    Create WebSocketManager.js

---
+++ src/websocket/WebSocketManager.js
@@ -0,0 +1,38 @@
+const EventEmitter = require('events');
+const WebSocketConnection = require('./WebSocketConnection');
+const { Events, Status } = require('../../utils/Constants');
+
+class WebSocketManager extends EventEmitter {
+    constructor(client) {
+        super();
+        this.client = client;
+        this.connection = null;
+    }
+    
+    debug(message) {
+        return this.client.emit(Events.DEBUG, message);   
+    }
+    
+    connect(gateway) {
+        if (!this.connection) {
+          this.connection = new WebSocketConnection(this, gateway);
+          return true;
+        }
+        switch (this.connection.status) {
+            case Status.IDLE:
+            case Status.DISCONNECTED:
+              this.connection.connect(gateway, 5500);
+              return true;
+            default:
+              this.debug(`Could not connect to ${gateway} as the websocket is ${this.connection.status}`);
+              return false;
+        }
+    }
+    
+    destroy() {
+        if (!this.connection) return this.debug('No connection to destroy.');
+        this.connection.destroy();
+    }
+}
+
+module.exports = WebSocketManager;
