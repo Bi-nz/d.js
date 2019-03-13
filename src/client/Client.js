2f7df9dac5b5bcf48c0ec87d5b1372770340bae3
Nuno135 <nuriel.taha@outlook.com>
2019-03-14 00:17:18 +0100

    Create Client.js

---
+++ src/client/Client.js
@@ -0,0 +1,32 @@
+const EventEmitter = require('events').EventEmitter;
+const ClientManager = require('./ClientManager');
+const WebSocketManager = require('./websocket/WebSocketManager');
+
+class Client extends EventEmitter {
+    constructor(options = {}) {
+        super();
+           this.manager = new ClientManager(this);
+
+           this.ws = new WebSocketManager(this);
+        
+           this.readyAt = null;
+        
+        // Gonna add more stuff here later
+    }
+    
+    get uptime() {
+        return this.readyAt ? Date.now() - this.readyAt : null;    
+    }
+    
+    login(token) {
+          return new Promise((resolve, reject) => {
+          if (!token || typeof token !== 'string') throw new Error('TOKEN_INVALID');
+          this.manager.connectToWebSocket(token, resolve, reject);
+        }).catch(e => {
+          this.destroy();
+          return Promise.reject(e);
+        });
+    }
+};
+
+module.exports = Client;
