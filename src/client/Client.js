2cf138e5cd56bb926a851fc297e7fc4bc6254046
Nuno135 <nuriel.taha@outlook.com>
2019-03-14 21:55:57 +0100

    Added gateway stuff

--- src/client/Client.js
+++ src/client/Client.js
@@ -33,9 +33,10 @@ class Client extends EventEmitter {
         return new Promise((resolve, reject) => {
             this.emit(Events.DEBUG, `Using token: ${token}`);
             res = JSON.parse(res);
-            const gateway = res.url;
+            let gateway = res.url; 
             this.emit(Events.DEBUG, `Gateway: ${gateway}`);
-            this.ws.connect(gateway)
+            gateway += 'v=6&encoding=json';
+            this.ws.connect(gateway);
         }).catch(e => {
          this.destroy();
           console.log(e);
